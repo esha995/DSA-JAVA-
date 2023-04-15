f04df650bd5333a0fbed0780ef1a435877831da3
esha995 <eshaaaagarwal996@gmail.com>
2023-04-16 04:11:00 +0530

    commit1

---
+++ print output/Helloworld.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Firstprogram{
+    public public static void name() {
+        System.out.println("helloworld");    // print in different line 
+        System.out.println("helloworld");
+        System.out.print("helloworld");       // doesnot print in different line
+        System.out.print("helloworld");
+        System.out.print("helloworld\n")      // also print in different line 
+    }
+}
---
+++ print output/Starz.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main{
+    public public static void name() {
+        System.out.println("*****");    
+        System.out.println("   *");
+        System.out.print("  *");       
+        System.out.print("*");
+        System.out.print("*****")      
+    }
+}
+
+
+// print z
+//     *****
+//         *
+//        *
+//      *
+//     ***** 
