df411c86bc006d20224c18ff5743c41283e48d95
esha995 <eshaaaagarwal996@gmail.com>
2023-06-16 13:43:21 +0530

    commit

---
+++ Loop/intro.java
@@ -0,0 +1,18 @@
+loop is excecuted in forward direction only
+
+- while()
+  intialize int i=0
+  condition(while(i<10)){
+    i++;#update
+  }
+///////////////////////////////////////////////////////////
+ - update
+ for()
+ for(int i=0;i<n;i++);
+////////////////////////////////////////////////////////////
+ -do while()
+ int i=0;
+ do{
+    i++;
+ }while()
+  
