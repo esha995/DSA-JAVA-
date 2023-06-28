659970ee35ec502bf8a6e315b7f7a1e20db39ff8
esha995 <eshaaaagarwal996@gmail.com>
2023-06-28 12:32:40 +0530

    commit

---
+++ Function/Bubble_Sort.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Bubble_Sort {
+    public static void bubbleSort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {-+
+
+                    Swap array[j] and array[j+1]
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 5, 9,4 , 3};
+        System.out.println("Array before sorting:");
+        printArray(array);
+
+        bubbleSort(array);
+
+        System.out.println("Array after sorting:");
+        printArray(array);
+    }
+
+    public static void printArray(int[] array) {
+        for (int element : array) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+
+}
